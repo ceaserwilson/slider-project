b013b464ec043eb8c9d8919b2a8ee89aa9704489
Chris-wilson Obidiebube <christianwilsonobi@gmail.com>
2023-10-20 14:10:06 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
+function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+  useEffect(() => {
+    const indexInterval = setInterval(() => {
+      setIndex(index + 1);
+    }, 4500);
+    return () => clearInterval(indexInterval);
+  }, [index]);
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIdx) => {
+          const { id, image, title, quote, name } = person;
+          // stuff
+          let position = "nextSlide";
+          if (personIdx === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIdx === index - 1 ||
+            (index === 0 && personIdx === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+          // i just noticed that or operator will return the two values if both are correct
+          return (
+            <article key={id} className={position}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
